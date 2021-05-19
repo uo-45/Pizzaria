586df4acf845166e6789a2513722c3c13c03043a
uo-45 <79004488+uo-45@users.noreply.github.com>
2021-05-19 18:56:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+menu_list_array = ["Veg Margherita Pizza",//add more items
+                    ];
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
